098c99a29653026fa799c1df6286fe4a3ba35ba2
xiaoqufengchao <xiaoqufengchao@163.com>
2018-03-12 01:58:22 -0700

    no shell

--- tst/test.sh
+++ tst/test.sh
@@ -20,6 +20,3 @@ str1=$(md5 $folder/CLM4.SCALE_FACTOR.JPL.MSCNv01CRIv01.nc)
 str2=$(cat $folder/CLM4.SCALE_FACTOR.JPL.MSCNv01CRIv01.nc.md5)
 python compare.py $str1 $str2
 if [ $? -gt 0 ] ; then echo "Problem Checking file" >&2 ; exit 44 ; fi
-
-
-$SHELL
