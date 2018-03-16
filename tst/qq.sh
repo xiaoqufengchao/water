baf017ea604ee053c20a44884cb960f4e810c420
xiaoqufengchao <xiaoqufengchao@163.com>
2018-03-15 23:30:10 -0700

    yo

--- tst/qq.sh
+++
@@ -1,7 +0,0 @@
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-     sh compare.sh hello
-else
-  echo "ELSE!"
-fi
-
-$SHELL
---
+++ tst/test.py
@@ -0,0 +1,13 @@
+import sys
+import os.path
+import subprocess
+import netCDF4
+import numpy
+import datetime
+import fiona
+import shapely.geometry
+import rtree
+import math
+import csv
+
+print "nice!"
