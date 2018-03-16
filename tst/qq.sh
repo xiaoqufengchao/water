6a92029c149816539035768f71e937f72cde644b
xiaoqufengchao <xiaoqufengchao@163.com>
2018-03-15 22:38:37 -0700

    ww

--- tst/qq.sh
+++ tst/qq.sh
@@ -1,5 +1,8 @@
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  echo "GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc: OK"
+     diff -w <(md5sum GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc) \
+		  		GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc.md5
+     if [ $? -gt 0 ] ; then echo "Problem checking retired GRACE" >&2 ; exit 44 ; fi
+     echo "GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc: OK"
 else
   echo "ELSE!"
 fi
