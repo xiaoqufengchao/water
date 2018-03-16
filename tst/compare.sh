d7276f6cae8901e59ca18f295db13c1fb7409a36
xiaoqufengchao <xiaoqufengchao@163.com>
2018-03-15 23:04:39 -0700

    ww

---
+++ tst/compare.sh
@@ -0,0 +1,5 @@
+file=$1
+
+diff -w (md5sum $1) $1.md5
+if [ $? -gt 0 ]; then echo "Problem checking retired GRACE" >&2 ; exit 44 ; fi
+echo "$file OK"
--- tst/qq.sh
+++ tst/qq.sh
@@ -1,8 +1,5 @@
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-     diff -w <(md5sum GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc) \
-		  		GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc.md5
-     if [ $? -gt 0 ] ; then echo "Problem checking retired GRACE" >&2 ; exit 44 ; fi
-     echo "GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc: OK"
+     sh compare.sh hello
 else
   echo "ELSE!"
 fi
