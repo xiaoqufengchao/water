281a9809d547b250657a7ed58a7d6874787ac9be
xiaoqufengchao <xiaoqufengchao@163.com>
2018-03-15 22:33:00 -0700

    qq

---
+++ tst/qq.sh
@@ -0,0 +1,10 @@
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  diff -w <(md5sum GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc) \
+		GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc.md5
+  if [ $? -gt 0 ] ; then echo "Problem checking retired GRACE" >&2 ; exit 44 ; fi
+  echo "GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc: OK"
+else
+  md5sum -c GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc.md5
+fi
+
+$SHELL
--- tst/tst_pub_dwnl_David_etal_201x_SER.sh
+++ tst/tst_pub_dwnl_David_etal_201x_SER.sh
@@ -125,6 +125,7 @@ done
 #-------------------------------------------------------------------------------
 cd $folder
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  echo "WTF AM I IN HERE?"
   diff -w <(md5sum GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc) \
 		GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc.md5
   if [ $? -gt 0 ] ; then echo "Problem checking retired GRACE" >&2 ; exit 44 ; fi
