70160261b11c52edcb1044a5e80eb63117dadb08
xiaoqufengchao <xiaoqufengchao@163.com>
2018-03-17 16:51:06 -0700

    ..

--- tst/test.py
+++ tst/test.py
@@ -1,8 +1,8 @@
 import sys
-sys.path.append(r'C:\ProgramData\Miniconda2\Lib\site-packages')
+print(sys.path)
 import os.path
 import subprocess
-import netCDF4
+#import netCDF4
 import numpy
 import datetime
 import fiona
