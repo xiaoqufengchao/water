c95c18d4897b8dc732e12f911839b2e10927b0de
xiaoqufengchao <xiaoqufengchao@163.com>
2018-03-17 17:20:15 -0700

    ..

--- tst/test.py
+++ tst/test.py
@@ -3,8 +3,8 @@ sys.path.append(r'C:\ProgramData\Miniconda2\Lib\site-packages')
 print(sys.path)
 import os.path
 import subprocess
-import netCDF4
-import numpy
+#import netCDF4
+#import numpy
 import datetime
 import fiona
 import shapely.geometry
