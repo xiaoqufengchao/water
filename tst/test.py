acf241abaacef7380c6a750febe5dbb71fe94361
xiaoqufengchao <xiaoqufengchao@163.com>
2018-03-17 20:27:40 -0700

    ww

--- tst/test.py
+++ tst/test.py
@@ -1,11 +1,11 @@
 import sys
 import os.path
 import subprocess
-#import netCDF4
-#import numpy
+import netCDF4
+import numpy
 import datetime
-#import fiona
-#import shapely.geometry
+import fiona
+import shapely.geometry
 import rtree
 import math
 import csv
